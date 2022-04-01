e3319b243f671b7b0e8b97aab86f673eb0c026a4
Wensi Xu <xuwen@Macbook-Pro.local>
2022-03-31 23:23:09 -0700

    add hello world ios

---
+++ helloios/helloios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Wensi Xu on 3/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
